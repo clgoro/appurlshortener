b92292f0e6bee7a5bab19649d556024416e85dcb
clgoro <clgoro@gmail.com>
2017-11-25 15:47:04 -0500

    Added gem for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+	root 'urls#index'
   resources :urls
   get '/:id' => 'urls#forward'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171030220153) do
 
-  create_table "models", force: :cascade do |t|
-    t.string "url"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "urls", force: :cascade do |t|
     t.string "address"
     t.datetime "created_at", null: false
